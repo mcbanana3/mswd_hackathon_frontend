493679d08c55cc6fda42e2957e60675f5eac5974
Aryan Yalavarthi <2300030049@kluniversity.in>
2024-12-08 02:05:39 +0530

    Initial Frontend Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,78 @@
+import { Button, Flex, Box, Link, useColorMode } from "@chakra-ui/react";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import userAtom from "../atoms/userAtom";
+import { AiFillHome } from "react-icons/ai";
+import { RxAvatar } from "react-icons/rx";
+import { Link as RouterLink } from "react-router-dom";
+import { FiLogOut } from "react-icons/fi";
+import useLogout from "../hooks/useLogout";
+import authScreenAtom from "../atoms/authAtom";
+import { BsFillChatQuoteFill } from "react-icons/bs";
+import { MdOutlineSettings } from "react-icons/md";
+import CreatePost from "./CreatePost";
+
+const Header = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const user = useRecoilValue(userAtom);
+  const logout = useLogout();
+  const setAuthScreen = useSetRecoilState(authScreenAtom);
+
+  return (
+    <Flex justifyContent={"space-between"} mt={6} mb="12">
+      {user && (
+        <Link as={RouterLink} to="/">
+          <AiFillHome size={24} />
+        </Link>
+      )}
+      {!user && (
+        <Link
+          as={RouterLink}
+          to={"/auth"}
+          onClick={() => setAuthScreen("login")}
+        >
+          Login
+        </Link>
+      )}
+
+      <Box
+        cursor="pointer"
+        fontWeight="bold"
+        fontSize="lg"
+        color={colorMode === "dark" ? "white" : "black"}
+        onClick={toggleColorMode}
+      >
+        Connectify
+      </Box>
+
+      {user && (
+        <Flex alignItems={"center"} gap={4}>
+          <Link as={RouterLink} to={`/${user.username}`}>
+            <RxAvatar size={24} />
+          </Link>
+          <CreatePost />
+          <Link as={RouterLink} to={`/chat`}>
+            <BsFillChatQuoteFill size={20} />
+          </Link>
+          <Link as={RouterLink} to={`/settings`}>
+            <MdOutlineSettings size={20} />
+          </Link>
+          <Button size={"xs"} onClick={logout}>
+            <FiLogOut size={20} />
+          </Button>
+        </Flex>
+      )}
+
+      {!user && (
+        <Link
+          as={RouterLink}
+          to={"/auth"}
+          onClick={() => setAuthScreen("signup")}
+        >
+          Sign up
+        </Link>
+      )}
+    </Flex>
+  );
+};
+
+export default Header;
